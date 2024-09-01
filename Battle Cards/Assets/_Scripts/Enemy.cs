899de952b24ca46194139029c260e71312a9f501
Saken5676 <ptwilliams5676@gmail.com>
2024-09-01 08:38:00 -0500

    Update
    Testing changing a script for git

--- Battle Cards/Assets/_Scripts/Enemy.cs
+++ Battle Cards/Assets/_Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //this does nothing
     }
 
     // Update is called once per frame
